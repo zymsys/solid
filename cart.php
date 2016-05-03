1112a7750ded041f54cca419b80324c75e49d9e4
Vic Metcalfe <vic@acm.org>
2016-05-03 06:56:44 -0400

    Code moved into functions

--- cart.php
+++ cart.php
@@ -1,14 +1,25 @@
 <?php
-session_start();
-$connection = new \PDO('mysql:host=localhost;dbname=solid', 'root', '');
-if (!isset($_SESSION['cart'])) {
-    $connection->exec("INSERT INTO cart () VALUES ()");
-    $_SESSION['cart'] = $connection->lastInsertId();
+function initialize()
+{
+    global $connection;
+
+    session_start();
+    $connection = new \PDO('mysql:host=localhost;dbname=solid', 'root', '');
+    if (!isset($_SESSION['cart'])) {
+        $connection->exec("INSERT INTO cart () VALUES ()");
+        $_SESSION['cart'] = $connection->lastInsertId();
+    }
 }
 
-if (isset($_POST['addproduct'])) {
+function handleAdd()
+{
+    global $connection;
+
+    if (!isset($_POST['addproduct'])) {
+        return;
+    }
     $sql = "INSERT INTO cartitem (cart, product, quantity) VALUES (:cart, :product, :quantity)
-              ON DUPLICATE KEY UPDATE quantity = quantity + :quantity";
+          ON DUPLICATE KEY UPDATE quantity = quantity + :quantity";
     $parameters = [
         'cart' => $_SESSION['cart'],
         'product' => $_POST['addproduct'],
@@ -18,7 +29,13 @@ if (isset($_POST['addproduct'])) {
     $statement->execute($parameters);
 }
 
-if (isset($_POST['update'])) {
+function handleUpdate()
+{
+    global $connection;
+
+    if (!isset($_POST['update'])) {
+        return;
+    }
     $sql = "UPDATE cartitem SET quantity=:quantity WHERE cart=:cart and product=:product";
     $parameters = [
         'cart' => $_SESSION['cart'],
@@ -29,9 +46,89 @@ if (isset($_POST['update'])) {
     $statement->execute($parameters);
 }
 
-$statement = $connection->prepare("SELECT * FROM cartitem WHERE cart=:cart AND quantity <> 0");
-$statement->execute(['cart' => $_SESSION['cart']]);
-$cartItems = $statement->fetchAll();
+function loadCartItems()
+{
+    global $connection, $viewData;
+
+    $viewData = [];
+    $statement = $connection->prepare("SELECT * FROM cartitem WHERE cart=:cart AND quantity <> 0");
+    $statement->execute(['cart' => $_SESSION['cart']]);
+    return $statement->fetchAll();
+}
+
+function loadProducts()
+{
+    global $connection;
+
+    $products = [];
+    $result = $connection->query("SELECT * FROM product");
+    foreach ($result as $product) {
+        $products[$product['id']] = $product;
+    }
+    return $products;
+}
+
+function loadProvinces()
+{
+    global $connection;
+
+    $provinces = [];
+    $result = $connection->query("SELECT * FROM province ORDER BY name");
+    foreach ($result as $row) {
+        $provinces[$row['code']] = $row;
+    }
+    return $provinces;
+}
+
+function calculateCartSubtotal($cartItems, $products)
+{
+    $subtotal = 0;
+
+    foreach ($cartItems as $cartItem) {
+        $product = $products[$cartItem['product']];
+        $subtotal += $cartItem['quantity'] * $product['price'];
+    }
+
+    return $subtotal;
+}
+
+function calculateCartTaxes($cartItems, $products, $taxrate)
+{
+    $taxable = 0;
+
+    foreach ($cartItems as $cartItem) {
+        $product = $products[$cartItem['product']];
+        $taxable += $product['taxes'] ? $cartItem['quantity'] * $product['price'] : 0;
+    }
+    return $taxable * $taxrate / 100;
+}
+
+function buildViewData()
+{
+    $viewData =  [
+        'cartItems' => loadCartItems(),
+        'products' => loadProducts(),
+        'provinces' => loadProvinces(),
+        'provinceCode' => isset($_GET['province']) ? $_GET['province'] : 'ON', //Default to GTA-PHP's home
+    ];
+
+    foreach ($viewData['provinces'] as $province) {
+        if ($province['code'] === $viewData['provinceCode']) {
+            $viewData['province'] = $province;
+        }
+    }
+
+    $viewData['subtotal'] = calculateCartSubtotal($viewData['cartItems'], $viewData['products']);
+    $viewData['taxes'] = calculateCartTaxes($viewData['cartItems'], $viewData['products'], $viewData['province']['taxrate']);
+    $viewData['total'] = $viewData['subtotal'] + $viewData['taxes'];
+
+    return $viewData;
+}
+initialize();
+handleAdd();
+handleUpdate();
+
+$viewData = buildViewData();
 ?>
 <!doctype html>
 <html lang="en">
@@ -54,12 +151,7 @@ $cartItems = $statement->fetchAll();
             <th>Group Gets</th>
             <th><!-- Column for add to cart button --></th>
         </tr>
-        <?php
-        $products = [];
-        $result = $connection->query("SELECT * FROM product");
-        foreach ($result as $product) {
-            $products[$product['id']] = $product;
-            ?>
+            <?php foreach ($viewData['products'] as $product): ?>
             <tr>
                 <td><?php echo $product['name']; ?></td>
                 <td><?php
@@ -75,28 +167,13 @@ $cartItems = $statement->fetchAll();
                     </form>
                 </td>
             </tr>
-            <?php
-        }
-        ?>
+            <?php endforeach; ?>
     </table>
-    <?php if (count($cartItems) > 0): ?>
-        <?php
-        $total = 0;
-
-        $provinceCode = isset($_GET['province']) ? $_GET['province'] : 'ON'; //Default to GTA-PHP's home
-        $provinces = [];
-        $result = $connection->query("SELECT * FROM province ORDER BY name");
-        foreach ($result as $row) {
-            $provinces[$row['code']] = $row;
-            if ($row['code'] === $provinceCode) {
-                $province = $row;
-            }
-        }
-        ?>
+    <?php if (count($viewData['cartItems']) > 0): ?>
         <h2>Your Cart:</h2>
         <table class="table">
-            <?php foreach ($cartItems as $cartItem): ?>
-                <?php $product = $products[$cartItem['product']]; ?>
+            <?php foreach ($viewData['cartItems'] as $cartItem): ?>
+                <?php $product = $viewData['products'][$cartItem['product']]; ?>
                 <tr>
                     <td>
                         <?php echo $product['name']; ?>
@@ -113,7 +190,6 @@ $cartItems = $statement->fetchAll();
                     <td>
                         <?php
                         echo number_format($cartItem['quantity'] * $product['price'] / 100, 2);
-                        $total += $cartItem['quantity'] * $product['price'];
                         ?>
                     </td>
                 </tr>
@@ -121,33 +197,31 @@ $cartItems = $statement->fetchAll();
             <tr>
                 <td><!-- Name --></td>
                 <td style="text-align: right">Subtotal:</td>
-                <td><?php echo number_format($total / 100, 2); ?></td>
+                <td><?php echo number_format($viewData['subtotal'] / 100, 2); ?></td>
             </tr>
             <tr>
                 <td><!-- Name --></td>
                 <td style="text-align: right">
-                    <?php echo $province['name']; ?> taxes at
-                    <?php echo $province['taxrate'] ?>%:</td>
+                    <?php echo $viewData['province']['name']; ?> taxes at
+                    <?php echo $viewData['province']['taxrate'] ?>%:</td>
                 <td>
                     <?php
-                    $taxes = $total * $province['taxrate'] / 100;
-                    $total += $taxes;
-                    echo number_format($total / 100, 2);
+                    echo number_format($viewData['taxes'] / 100, 2);
                     ?>
                 </td>
             </tr>
             <tr>
                 <td><!-- Name --></td>
                 <td style="text-align: right">Total:</td>
-                <td><?php echo number_format($total / 100, 2); ?></td>
+                <td><?php echo number_format($viewData['total'] / 100, 2); ?></td>
             </tr>
         </table>
         <form action="cart.php" method="get">
             Calculate taxes for purchase from:
             <select name="province">
-                <?php foreach ($provinces as $province): ?>
+                <?php foreach ($viewData['provinces'] as $province): ?>
                     <?php
-                    $selected = $provinceCode === $province['code'] ? 'selected' : '';
+                    $selected = $viewData['provinceCode'] === $province['code'] ? 'selected' : '';
                     ?>
                     <option value="<?php echo $province['code']; ?>" <?php echo $selected; ?>>
                         <?php echo $province['name']; ?>
@@ -157,15 +231,15 @@ $cartItems = $statement->fetchAll();
             <button type="submit" class="btn btn-default btn-xs">Recalculate</button>
         </form>
         <form action="checkout.php" method="post">
-            <?php foreach ($cartItems as $itemNumber => $cartItem): ?>
+            <?php foreach ($viewData['cartItems'] as $itemNumber => $cartItem): ?>
                 <?php
-                $product = $products[$cartItem['product']];
+                $product = $viewData['products'][$cartItem['product']];
                 ?>
                 <input type="hidden" name="item<?php echo $itemNumber; ?>"
                        value="<?php echo $product['name'] . '|' . number_format($product['price'] / 100, 2); ?>">
             <?php endforeach; ?>
-            <input type="hidden" name="item<?php echo count($cartItems); ?>"
-                   value="<?php echo 'Tax|' . number_format($taxes / 100, 2); ?>">
+            <input type="hidden" name="item<?php echo count($viewData['cartItems']); ?>"
+                   value="<?php echo 'Tax|' . number_format($viewData['taxes'] / 100, 2); ?>">
             <button type="submit" class="btn btn-primary" style="float: right">Checkout</button>
         </form>
     <?php endif; ?>
